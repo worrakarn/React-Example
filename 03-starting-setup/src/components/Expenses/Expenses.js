f70754c77d0be3fa21ebdf3f3b635087487a73da
TVDIRECT\worrakarn_p <worrakarn.palawat@abpo.co.th>
2023-10-25 17:28:21 +0700

    Example

---
+++ 03-starting-setup/src/components/Expenses/Expenses.js
@@ -0,0 +1,55 @@
+import { useState } from "react"
+import "./Expenses.css"
+import ExpensesFilter from "./ExpensesFilter"
+import ExpensesList from "./ExpensesList"
+import ExpensesChart from './ExpensesChart';
+import Card from "../UI/Card"
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020")
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
+  // let expensesContent = <p>No expenses found.</p>
+
+  // if (filteredExpenses.length > 0) {
+  //   expensesContent = filteredExpenses.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ))
+  // }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
+      {/* {expensesContent} */}
+      {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+      {filteredExpenses.length > 0 &&
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          ></ExpenseItem>
+        ))} */}
+    </Card>
+  )
+}
+
+export default Expenses
